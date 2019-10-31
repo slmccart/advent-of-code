b424d75db19fe555e6d8ae2e054a356827c609c5
Scott McCarthy <smccarthy@shelltechworks.com>
2019-10-31 14:13:05 -0400

    Create Present from "NxNxN" formatted string

--- 2015/02_i_was_told_there_would_be_no_math/src/main.rs
+++ 2015/02_i_was_told_there_would_be_no_math/src/main.rs
@@ -22,11 +22,21 @@ impl Present {
     fn amount_of_wrapping_paper_needed(&self) -> u32 {
         self.surface_area() + self.area_of_smallest_side()
     }
+
+    fn new(input: &str) -> Present {
+        let v: Vec<&str> = input.splitn(3, 'x').collect();
+        Present {
+            width:  v[0].parse::<u32>().unwrap(), 
+            length: v[1].parse::<u32>().unwrap(), 
+            height: v[2].parse::<u32>().unwrap()
+        }
+    }
 }
 
 fn main() {
-    let present1 =  Present { length: 2, width: 3, height: 4 };
+    let present1 = Present { length: 2, width: 3, height: 4 };
     let present2 = Present { length: 1, width: 1, height: 10 };
+    let present3 = Present::new("2x3x4");
 
     println!("Volume of present1 is: {}", present1.volume());
     println!("Surface area of present1 is: {}", present1.surface_area());
@@ -37,4 +47,9 @@ fn main() {
     println!("Surface area of present2 is: {}", present2.surface_area());
     println!("Surface area of smallest side of present2 is: {}", present2.area_of_smallest_side());
     println!("Amount of wrapping paper needed for present2 is: {}", present2.amount_of_wrapping_paper_needed());
+
+    println!("Volume of present3 is: {}", present3.volume());
+    println!("Surface area of present3 is: {}", present3.surface_area());
+    println!("Surface area of smallest side of present3 is: {}", present3.area_of_smallest_side());
+    println!("Amount of wrapping paper needed for present3 is: {}", present3.amount_of_wrapping_paper_needed());
 }
