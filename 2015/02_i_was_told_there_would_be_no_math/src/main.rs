6db91edebd2d74890bc4d2b34e417e7ff5b4e1d6
Scott McCarthy <smccarthy@shelltechworks.com>
2019-10-31 15:00:22 -0400

    Functions for computing amount of ribbon needed for Present

--- 2015/02_i_was_told_there_would_be_no_math/src/main.rs
+++ 2015/02_i_was_told_there_would_be_no_math/src/main.rs
@@ -8,7 +8,6 @@ struct Present {
 }
 
 impl Present {
-    #[allow(dead_code)]
     fn volume(&self) -> u32 {
         self.length * self.width * self.height
     }
@@ -21,8 +20,16 @@ impl Present {
         cmp::min(self.length * self.width, cmp::min(self.width * self.height, self.height * self.length))
     }
 
+    fn perimeter_of_smallest_side(&self) -> u32 {
+        cmp::min(2 * self.length + 2 * self.width, cmp::min(2 * self.width + 2 * self.height, 2 * self.height + 2 * self.length))
+    }
+
     fn amount_of_wrapping_paper_needed(&self) -> u32 {
         self.surface_area() + self.area_of_smallest_side()
+    }
+
+    fn length_of_ribbon_needed(&self) -> u32 {
+        self.perimeter_of_smallest_side() + self.volume()
     }
 
     fn new(input: &str) -> Present {
@@ -36,6 +43,20 @@ impl Present {
 }
 
 fn main() {
+    let present1 = Present::new("2x3x4");
+    let present2 = Present::new("1x1x10");
+
+    println!("Ribbon for present1 - Smallest side: {} - Bow: {} - Total: {}", 
+        present1.perimeter_of_smallest_side(), 
+        present1.volume(), 
+        present1.length_of_ribbon_needed());
+
+    println!("Ribbon for present2 - Smallest side: {} - Bow: {} - Total: {}", 
+        present2.perimeter_of_smallest_side(), 
+        present2.volume(), 
+        present2.length_of_ribbon_needed());
+
+
     let presents = read_input("input.txt");
     let presents: Vec<&str> = presents.split('\n').collect();
 
