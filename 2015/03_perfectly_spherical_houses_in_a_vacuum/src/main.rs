a95b203f400bc1af776b63c6871e687574d75732
Scott McCarthy <smccarthy@shelltechworks.com>
2019-11-11 15:06:03 -0500

    Day 3 - Deliver presents

---
+++ 2015/03_perfectly_spherical_houses_in_a_vacuum/src/main.rs
@@ -0,0 +1,49 @@
+use std::collections::HashMap;
+use std::fs;
+
+fn main() {
+    let directions = read_input("input.txt");
+    let map = deliver_to_list_of_directions(&directions);
+
+    //println!("{:?}", map);
+    println!("\nmap has {} houses with at least one present\n", map.len());
+}
+
+fn move_and_deliver_present(map: &mut HashMap<(i32, i32), u32>, current_location: (i32, i32), direction: char) -> (i32, i32) {
+    let new_location = 
+        match direction {
+            '^' => (current_location.0 + 1, current_location.1),
+            'v' => (current_location.0 - 1, current_location.1),
+            '<' => (current_location.0, current_location.1 -1),
+            '>' => (current_location.0, current_location.1 + 1),
+            _ => current_location
+        };
+
+    let presents = map.entry(new_location).or_insert(0);
+    *presents += 1;
+
+    new_location
+}
+
+fn deliver_to_list_of_directions(list_of_directions: &str) -> HashMap<(i32, i32), u32> {
+    let mut location = (0,0);
+    let mut map: HashMap<(i32, i32), u32> =  HashMap::new();
+
+    //Deliver present to initial house
+    map.insert(location, 1);
+
+    for direction in list_of_directions.chars() {
+        location = move_and_deliver_present(&mut map, location, direction);
+    }
+
+    //Deliver to two test houses
+    //location = move_and_deliver_present(&mut map, location, ">");
+    //location = move_and_deliver_present(&mut map, location, "<");
+
+    map
+}
+
+fn read_input(filename: &str) -> String {
+    fs::read_to_string(filename)
+        .expect("Something went wrong reading the file")
+}
