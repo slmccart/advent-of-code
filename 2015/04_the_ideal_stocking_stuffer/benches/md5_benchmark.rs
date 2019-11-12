ec2ea7708d01c09c06788288a0488af10f2c3329
Scott McCarthy <smccarthy@shelltechworks.com>
2019-11-12 16:27:54 -0500

    Benchmark 6 zero solution

--- 2015/04_the_ideal_stocking_stuffer/benches/md5_benchmark.rs
+++ 2015/04_the_ideal_stocking_stuffer/benches/md5_benchmark.rs
@@ -1,10 +1,16 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use md5lib::find_smallest_number;
+use md5lib::find_smallest_number_with_six_zeroes;
 
-pub fn criterion_benchmark(_c: &mut Criterion) {
+pub fn criterion_benchmark_five_zeroes(_c: &mut Criterion) {
     let mut c_prime = Criterion::default().sample_size(20);
-    c_prime.bench_function("Smallest with 00000", |b| b.iter(|| find_smallest_number(black_box("iwrupvqb")));
+    c_prime.bench_function("Smallest with 00000", |b| b.iter(|| find_smallest_number(black_box("iwrupvqb"))));
 }
 
-criterion_group!(benches, criterion_benchmark);
+pub fn criterion_benchmark_six_zeroes(_c: &mut Criterion) {
+    let mut c_prime = Criterion::default().sample_size(10);
+    c_prime.bench_function("Smallest with 000000", |b| b.iter(|| find_smallest_number_with_six_zeroes(black_box("iwrupvqb"))));
+}
+
+criterion_group!(benches, criterion_benchmark_five_zeroes, criterion_benchmark_six_zeroes);
 criterion_main!(benches);
