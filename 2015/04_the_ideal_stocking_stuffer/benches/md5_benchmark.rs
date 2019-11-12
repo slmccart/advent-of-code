99e9b8894c9b83b07cc601c7762a62337374b8ce
Scott McCarthy <smccarthy@shelltechworks.com>
2019-11-12 15:43:22 -0500

    Stable benchmark

--- 2015/04_the_ideal_stocking_stuffer/benches/md5_benchmark.rs
+++ 2015/04_the_ideal_stocking_stuffer/benches/md5_benchmark.rs
@@ -1,8 +1,9 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-mod lib;
+use md5lib::find_smallest_number;
 
-pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| lib::find_smallest_number(black_box(20))));
+pub fn criterion_benchmark(_c: &mut Criterion) {
+    let mut c_prime = Criterion::default().sample_size(10);
+    c_prime.bench_function("Smallest with 00000", |b| b.iter(|| find_smallest_number(black_box("iwrupvqb"), black_box("00000"))));
 }
 
 criterion_group!(benches, criterion_benchmark);
