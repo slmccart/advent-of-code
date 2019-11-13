c5a01497b98c53e1bb466e9392999b7a2e0a76c5
Scott McCarthy <smccarthy@shelltechworks.com>
2019-11-13 14:35:02 -0500

    First attempt at Day 5 part 2, doesn't pass all tests

--- 2015/05_doesnt_he_have_intern-elves_for_this/src/main.rs
+++ 2015/05_doesnt_he_have_intern-elves_for_this/src/main.rs
@@ -1,15 +1,21 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn main() {
     let strings = read_input("input.txt");
     let strings: Vec<&str> = strings.split('\n').collect();
     let mut nice_strings = 0;
+    let mut nice_strings_new_rules = 0;
 
     for string in strings {
         if is_nice_string(string) { nice_strings += 1 }
+        if is_nice_string_by_new_rules(string) { nice_strings_new_rules += 1 }
     }
 
     println!("Found {} nice strings", nice_strings);
+    println!("Found {} nice strings by the new rules", nice_strings_new_rules);
+
+    contains_letter_sandwich("abcdefeghi");
 }
 
 fn read_input(filename: &str) -> String {
@@ -19,6 +25,10 @@ fn read_input(filename: &str) -> String {
 
 fn is_nice_string(s: &str) -> bool {
     contains_at_least_three_vowels(s) && contains_double_letters(s) && !contains_naughty_substring(s)
+}
+
+fn is_nice_string_by_new_rules(s: &str) -> bool {
+    contains_two_independent_pairs(s) && contains_letter_sandwich(s)
 }
 
 fn contains_at_least_three_vowels(s: &str) -> bool {
@@ -64,6 +74,62 @@ fn contains_naughty_substring(s: &str) -> bool {
     false
 }
 
+fn contains_two_independent_pairs(s: &str) -> bool {
+    // xxyxx
+    // Windows(2): xx xy yx xx
+    
+    // aaa
+    // Windows(2): aa aa
+
+
+    let mut pairs = HashMap::new();
+
+    //Chunk the original string into pairs of characters starting at position 0
+    // This will get the pairs that start at an even index
+    //   abcdef becomes ab cd ef
+    let characters = s.chars().collect::<Vec<char>>();
+    let chunks = characters.chunks_exact(2);
+
+    for chunk in chunks {
+        let pair = pairs.entry(chunk).or_insert(0);
+        *pair += 1;
+
+        if *pair == 2 { return true; }
+    }
+
+    //Reset HashMap for next pass through the string
+    pairs.clear();
+
+    //Now chunk the original string into pairs of characters starting at position 1
+    // This will get the pairs that start at an odd index
+    //   abcdef becomes bc de f
+    let characters = s[1..].chars().collect::<Vec<char>>();
+    let chunks = characters.chunks_exact(2);
+
+    for chunk in chunks {
+        let pair = pairs.entry(chunk).or_insert(0);
+        *pair += 1;
+
+        if *pair == 2 { return true; }
+    }
+
+    false
+}
+
+fn contains_letter_sandwich(s: &str) -> bool {
+    //Get an iterator of rolling Windows containing exactly 3 characters each
+    // abcdefeghi becomes abc bcd cde def efe ...
+    let characters = s.chars().collect::<Vec<char>>();
+    let windows = characters.windows(3);
+
+    for window in windows {
+        //If the window contains the same character in the 1st and 3rd positions, then this string is a letter sandwich
+        if window[0] == window[2] { return true; }
+    }
+
+    false
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -98,5 +164,29 @@ mod tests {
         assert_eq!(super::is_nice_string("jchzalrnumimnmhp"), false);
         assert_eq!(super::is_nice_string("haegwjzuvuyypxyu"), false);
         assert_eq!(super::is_nice_string("dvszwmarrgswjxmb"), false);
+    }
+
+    #[test]
+    fn contains_two_independent_pairs() {
+        assert_eq!(super::contains_two_independent_pairs("xyxy"), true);
+        assert_eq!(super::contains_two_independent_pairs("xxyxx"), true);
+        assert_eq!(super::contains_two_independent_pairs("aabcdefgaa"), true);
+        assert_eq!(super::contains_two_independent_pairs("aaa"), false);
+    }
+
+    #[test]
+    fn contains_letter_sandwich() {
+        assert_eq!(super::contains_letter_sandwich("xyx"), true);
+        assert_eq!(super::contains_letter_sandwich("abcdefeghi"), true);
+        assert_eq!(super::contains_letter_sandwich("aaa"), true);
+        assert_eq!(super::contains_letter_sandwich("abc"), false);
+    }
+
+    #[test]
+    fn is_nice_string_by_new_rules() {
+        assert_eq!(super::is_nice_string_by_new_rules("qjhvhtzxzqqjkmpb"), true);
+        assert_eq!(super::is_nice_string_by_new_rules("xxyxx"), true);
+        assert_eq!(super::is_nice_string_by_new_rules("uurcxstgmygtbstg"), false);
+        assert_eq!(super::is_nice_string_by_new_rules("ieodomkazucvgmuy"), false);
     }
 }
