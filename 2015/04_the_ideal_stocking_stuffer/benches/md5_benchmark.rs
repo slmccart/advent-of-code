c7e2611d4933bc1f9c7177447d95c72ab0b56362
Scott McCarthy <smccarthy@shelltechworks.com>
2019-11-13 11:40:12 -0500

    Preferred method for configuring a Criterion

--- 2015/04_the_ideal_stocking_stuffer/benches/md5_benchmark.rs
+++ 2015/04_the_ideal_stocking_stuffer/benches/md5_benchmark.rs
@@ -2,14 +2,18 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use md5lib::find_smallest_number;
 use md5lib::find_smallest_number_with_six_zeroes;
 
-pub fn criterion_benchmark_five_zeroes(_c: &mut Criterion) {
-    let mut c_prime = Criterion::default().sample_size(20);
-    c_prime.bench_function("Smallest with 00000", |b| b.iter(|| find_smallest_number(black_box("iwrupvqb"))));
+pub fn criterion_benchmark_five_zeroes(c: &mut Criterion) {
+    let mut group = c.benchmark_group("criterion_benchmark_five_zeroes");
+    group.sample_size(20);
+    group.bench_function("Smallest with 00000", |b| b.iter(|| find_smallest_number(black_box("iwrupvqb"))));
+    group.finish();
 }
 
-pub fn criterion_benchmark_six_zeroes(_c: &mut Criterion) {
-    let mut c_prime = Criterion::default().sample_size(10);
-    c_prime.bench_function("Smallest with 000000", |b| b.iter(|| find_smallest_number_with_six_zeroes(black_box("iwrupvqb"))));
+pub fn criterion_benchmark_six_zeroes(c: &mut Criterion) {
+    let mut group = c.benchmark_group("criterion_benchmark_six_zeroes");
+    group.sample_size(10);
+    group.bench_function("Smallest with 000000", |b| b.iter(|| find_smallest_number_with_six_zeroes(black_box("iwrupvqb"))));
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark_five_zeroes, criterion_benchmark_six_zeroes);
